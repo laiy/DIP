9464053b09e34848be04ed4c32402c6fa53d6cb1
LY <ly.franky@gmail.com>
2015-10-21 10:27:56 +0800

    fucking scale function

---
+++ scale.m
@@ -0,0 +1,22 @@
+function scale(imagePath, scaleWidth, scaleHeight)
+    scaledImage = zeros(scaleHeight, scaleWidth);
+    originalImage = imread(imagePath);
+    [height, width] = size(originalImage);
+    widthScaleRate = scaleWidth / width;
+    heightScaleRate = scaleHeight / height;
+    for i = 1 : scaleHeight
+        for j = 1 : scaleWidth
+            pixel = [i / heightScaleRate, j / widthScaleRate];
+            y = pixel(1) - floor(pixel(1));
+            x = pixel(2) - floor(pixel(2));
+            leftTop = [floor(pixel(1)), floor(pixel(2))];
+            rightTop = [floor(pixel(1)), ceil(pixel(2))];
+            leftBottom = [ceil(pixel(1)), floor(pixel(2))];
+            rightBottom = [ceil(pixel(1)), ceil(pixel(2))];
+            scaledImage(i,j) = (1 - x) * (1 - y) * originalImage(leftTop(1), leftTop(2))+ ...
+                  x * (1 - y) * originalImage(rightTop(1), rightTop(2))+ ...
+                  (1 - x) * y * originalImage(leftBottom(1), leftBottom(2))+ ...
+                  x * y * originalImage(rightBottom(1), rightBottom(2));
+        end
+    end
+    imshow(uint8(scaledImage));
